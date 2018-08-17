05b80a341f9e018c1120928ba067aed348271841
Perry Tran <ptran209@gmail.com>
2018-08-17 15:16:09 -0700

    added room

--- hotel.js
+++ hotel.js
@@ -1,31 +1,29 @@
-
-
-class Hotel{
-	constructor(){
+class Hotel {
+	constructor() {
 		this.name = 'marriot';
 		this.rooms = [];
 		this.staff = [];
 	}
 	//make a customer object with the given name and add to a room.  need to add to a room with no occupants
-	checkInCustomer( name ){
+	checkInCustomer(name) {
 
 	}
-	checkOutCustomer(){
+	checkOutCustomer() {
 
 	}
 	//make a room and add to rooms array
-	addRoom(){
+	addRoom() {
 
 	}
 	//add staff to the hotel
-	hireStaff( name ) {
+	hireStaff(name) {
 
 	}
-	getVacantRooms(){
+	getVacantRooms() {
 
 	}
-	getKitchenService(){
-		
+	getKitchenService() {
+
 	}
 
 }
